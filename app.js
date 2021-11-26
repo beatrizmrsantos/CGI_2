1481ddaf3b149a77302826b60534fa065ee7bbcb
Beatriz <bmr.santos@campus.fct.unl.pt>
2021-11-26 12:12:09 +0000

    final

--- app.js
+++ app.js
@@ -11,32 +11,33 @@ import { scalem, translate } from "./libs/MV.js";
 /** @type WebGLRenderingContext */
 let gl;
 let program;
-      
-const pi =3.14
-const round = 360;
-let mode;                 // Drawing mode (gl.LINES or gl.TRIANGLES)
+
+const PI =3.14                    
+const ROUND = 360;          
+let mode;                          // Drawing mode (gl.LINES or gl.TRIANGLES)
 let rotationCannon = 0;   //rotacao do canhao do tanque no ecra
-let rotationHead = 0;    //rotacao da cabeca do tanque no ecra
-let movement = 0;       //tranlacao no ecra
-let zoom = 1.0;
-let x;                 //ponto do centro do cilindro do canhao
+let rotationHead = 0;         //rotacao da cabeca do tanque no ecra
+let movement = 0;         //tranlacao do tanque no ecra
+let zoom = 1.0;           
+let x;                       //ponto do centro do cilindro do canhão usado para calcular o vetor velocidade                                                       inicial de cada bala
 
-let shoot = false;
-let bullets = [];
+let shoot = false;     //Boolean para o clique do espaco para disparar o canhao
+let bullets = [];         //vetor de todas as balas existentes no programa
 
-//vetor acelaracao
-const a = (9.8)/800; 
-let t = translate(0,-a,0); 
-let acelaration = subtract(t,mat4());
-let scaleVelocity = scalem(0.2,0.2,0.2);
+const G = 9.8;         //valor da acelaracao com o tempo
+let t = translate(0,-G,0);    //matriz com a translação necessária para a realizao do vetor acelaracao
+let acelaration = subtract(t,mat4());    //vetor acelaracao final onde esta tudo a zero a exceção do y que tem o valor pretendido negativo de modo a simular a gravidade
+const escaleVelocity = scalem(4.5,4.5,4.5);  //escala para o vetor velocidade de modo a personaliza-la ao nosso gosto
+const time = scalem([1/60, 1/60, 1/60]);   //vetor tempo
 
 const DISTANCE = 5.0;
 
-let lookat1 = lookAt([-DISTANCE,0,0], [0,0,0], [0,1,0]);
+//lookAt das 4 vistas diferentes pedidas
+let lookat1 = lookAt([0,0,DISTANCE], [0,0,0], [0,1,0]);
 let lookat2 = lookAt([0,DISTANCE,0.0001], [0,0,0], [0,1,0]);
-let lookat3 = lookAt([0,0,DISTANCE], [0,0,0], [0,1,0]);
+let lookat3 = lookAt([DISTANCE,0,0], [0,0,0], [0,1,0]);
 let lookat4 = lookAt([DISTANCE,DISTANCE,DISTANCE], [0,0,0], [0,1,0]);
-let mView = lookat3;
+let mView = lookat1;
 
 //constantes de parametro dos objetos
 const TORUS_RADIUS = 0.7;
@@ -47,23 +48,26 @@ const CUBE_SIZE = 1;
 const WHEEL_SIZE = 1;
 const TANK_UPPERBODY_SIZE = 1;
 const HEAD_SIZE = 1;
-const SIZE_ANTENNA=1;
-const SIZE_CANNON= 1;
+const SIZE_ANTENNA = 1;
+const SIZE_CANNON = 1;
+const TRANSLACION = 0.01;  //velocidade da tranlacao do tanque todo no eixo dos x
+const ROTATION_HEAD = 2;   //rotacao do cilindro maior do tanque
+const ROTATION_CANNON = 1;  //rotacao up e down do canhao
 
-//size originais
+//tamanhos originais da roda
 const WHEEL_THICK = 1.5;
 const WHEEL_HEIGHT = 1;
 const SPHERE_SIZE = 0.4;
 
-//constantes tiles
+//distancia entre rodas do mesmo eixo
+const DISTANCE_WHEELS = 2;  
+
+//constantes dos tiles
 const TILE_HEIGHT = 1/6;
 const TILE_LENGHT = CUBE_SIZE;
 const TILE_WIDTH = CUBE_SIZE;
 
-//distancia entre rodas do mesmo eixo
-const DISTANCE_WHEELS = 2;
-
-//constantes do eixo
+//constantes do eixo entre as rodas
 const AXIS_WIDTH = WHEEL_SIZE*DISTANCE_WHEELS*2;
 const AXIS_HEIGHT = 0.25;
 const AXIS_WHEEL_SIZE = 0.6;
@@ -74,36 +78,36 @@ const WHEEL_AXIS_TRANSLATION_1 = WHEEL_SIZE*(TORUS_RADIUS*2)*AXIS_WHEEL_SIZE + 0
 const WHEEL_AXIS_TRANSLATION_2 = WHEEL_SIZE*(TORUS_RADIUS*4)*AXIS_WHEEL_SIZE + 0.2;
 const TRANSLATION_TOP_TILES = WHEEL_SIZE*AXIS_WHEEL_SIZE*TORUS_RADIUS + (TILE_HEIGHT)/2;
 
+//tranalcao para o corpo superior todo do tanque
+const TRANSLATION_TANK = WHEEL_SIZE*AXIS_WHEEL_SIZE*TORUS_RADIUS/2;
+
 //constantes tamanho cubo pequeno
 const TANK_LOWERBODY_LENGTH = DISTANCE_WHEELS*WHEEL_SIZE;
 const TANK_LOWERBODY_WIDTH = WHEEL_AXIS_TRANSLATION_2*2;
 const TANK_LOWERBODY_HEIGHT = WHEEL_SIZE*AXIS_WHEEL_SIZE*TORUS_RADIUS;
 
-
 //constantes tamanho cubo maior
 const TANK_UPPERBODY_WIDTH = 6*TANK_UPPERBODY_SIZE;
 const TANK_UPPERBODY_HEIGHT = 1.2*TANK_UPPERBODY_SIZE;
 const TANK_UPPERBODY_LENGHT = 3*TANK_UPPERBODY_SIZE;
-
-const TRANSLATION_TANK = WHEEL_SIZE*AXIS_WHEEL_SIZE*TORUS_RADIUS/2;
 const TRANSLATION_BODY_BIG = TANK_UPPERBODY_HEIGHT/2 + TANK_LOWERBODY_HEIGHT/2;
 
-// constantes head
+//constantes do cilindro maior do corpo do tanque 
 const HEAD_CYLINDER_HEIGHT=0.7*HEAD_SIZE;
 const HEAD_CYLINDER_RADIUS=2*HEAD_SIZE;
 const TRANSLATION_HEAD=TANK_UPPERBODY_HEIGHT + TANK_LOWERBODY_HEIGHT/2 +HEAD_CYLINDER_HEIGHT/2;
 
-//constantes da antena
+//constantes do cilindro menor do corpo do tanque
 const ANTENNA_HEIGHT = 0.3*SIZE_ANTENNA;
 const ANTENNA_RADIUS = 0.7*SIZE_ANTENNA;
 const TRANSLATION_ANTENNA = HEAD_CYLINDER_HEIGHT/2 + ANTENNA_HEIGHT/2 ;
 
-//constantes do pipe e cubo (canhao)
+//constantes do cano e cubo - canhao
 const CANNON_SQUARE = 0.4* SIZE_CANNON;
 const CANNON_PIPE_RADIUS = 1/6* SIZE_CANNON;
 const CANNON_HEIGHT = 2.6 *SIZE_CANNON;
 const TRANSLATION_SQUARE = HEAD_CYLINDER_RADIUS/2;
-const TRANSLATION_CANNON = CANNON_HEIGHT/2;
+const TRANSLATION_PIPE = CANNON_HEIGHT/2;
 
 
 function setup(shaders)
@@ -126,38 +130,38 @@ function setup(shaders)
     document.onkeydown = function(event) {
         switch(event.key) {
             case 'w':
-                if(rotationCannon>-60){
-                    rotationCannon -=1;
+                if(rotationCannon > -60){
+                    rotationCannon -= ROTATION_CANNON;
                 }
                 break;
             case 'W':
                 mode = gl.LINES;
                 break;
             case 's':
-                if(rotationCannon<7){
-                    rotationCannon +=1;
+                if(rotationCannon < 7){
+                    rotationCannon += ROTATION_CANNON;
                }
                 break;
             case 'S':
                 mode = gl.TRIANGLES;
                 break;
             case 'a':
-                rotationHead +=2;
+                rotationHead += ROTATION_HEAD;
                 break;
             case 'd':
-                rotationHead -=2;
+                rotationHead -= ROTATION_HEAD;
                 break;
             case ' ':
                 shoot = true;
                 break;
             case 'ArrowDown':
-                if(movement<8){
-                    movement+=0.05;
+                if(movement < 8){
+                    movement += TRANSLACION;
                 }
                 break;
             case 'ArrowUp':
-                if(movement>-8){
-                    movement-=0.05;
+                if(movement > -8){
+                    movement -= TRANSLACION;
                 }
                 break;
             case '1':
@@ -201,8 +205,7 @@ function setup(shaders)
         mProjection = ortho(-DISTANCE*aspect/zoom,DISTANCE*aspect/zoom, -DISTANCE/zoom, DISTANCE/zoom,-3*DISTANCE/zoom,3*DISTANCE/zoom);
     }
 
-    function resize_canvas(event)
-    {
+    function resize_canvas(){
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
 
@@ -219,7 +222,7 @@ function setup(shaders)
 
 
     //tanque todo
-    function tank(timestamp){
+    function tank(){
 
         multTranslation([movement,0,0]);
 
@@ -227,22 +230,22 @@ function setup(shaders)
             lowerBody();
         popMatrix();
             multTranslation([0, TRANSLATION_TANK, 0]);
-            upperBody(timestamp);
+            upperBody();
     }
 
     //corpo superior do tanque
-    function upperBody(timestamp){
+    function upperBody(){
         
         pushMatrix();
             body();
         popMatrix();
             multTranslation([0,TRANSLATION_HEAD,0]);
-            headSet(timestamp);
+            headSet();
 
     }
 
      //conjunto que roda
-     function headSet(timestamp){
+     function headSet(){
 
         multRotationY(rotationHead);
 
@@ -254,13 +257,13 @@ function setup(shaders)
             atenna();
         popMatrix();
             multTranslation([-TRANSLATION_SQUARE,0,0]);
-            canon(timestamp);
+            canon();
        
     }
 
 
     //ligacao cubo com cilindro
-    function canon(timestamp){
+    function canon(){
 
         multRotationZ(rotationCannon);
 
@@ -268,13 +271,13 @@ function setup(shaders)
             cube();
         popMatrix();
         pushMatrix();
-            multTranslation([-TRANSLATION_CANNON,0,0]);
+            multTranslation([-TRANSLATION_PIPE,0,0]);
             x = mult(inverse(mView), modelView());
             pipe();
         popMatrix();
 
         if(shoot){
-            addBullet(timestamp);
+            addBullet();
 
             shoot = false;
         }
@@ -282,14 +285,14 @@ function setup(shaders)
     }
 
 
-    function addBullet(timestamp){
+    function addBullet(){
 
         multTranslation([-CANNON_HEIGHT,0,0]);
 
         let pos = mult(inverse(mView), modelView());
-        let vel = mult(scaleVelocity, subtract(pos,x));
+        let vel = mult(escaleVelocity, subtract(pos,x));
 
-        bullets.push({posicao: pos, velocidade: vel, tempo: timestamp});
+        bullets.push({posicao: pos, velocidade: vel});
 
     }
 
@@ -305,7 +308,7 @@ function setup(shaders)
 
     }
 
-      //cilindro para disparar
+      //cilindro para disparar - cano
       function pipe(){
 
         multRotationZ(90);
@@ -316,7 +319,7 @@ function setup(shaders)
         CYLINDER.draw(gl, program, mode);
     }
 
-    //cubo de ligacao canhao e body
+    //cubo de ligacao do canhao e do body
     function cube(){
 
         multScale([CANNON_SQUARE,CANNON_SQUARE,CANNON_SQUARE]);
@@ -340,7 +343,7 @@ function setup(shaders)
         CYLINDER.draw(gl, program, mode);
     }
 
-    //cilindro grande
+    //cilindro grande em cima do tanque
     function head(){
 
         multScale([HEAD_CYLINDER_RADIUS,HEAD_CYLINDER_HEIGHT,HEAD_CYLINDER_RADIUS]);
@@ -353,7 +356,7 @@ function setup(shaders)
     }
 
 
-    //dois cubos juntos
+    //dois cubos juntos, o pequeno e o maior simulando o corpo do tanque
     function body(){
         
         multRotationY(90);
@@ -419,7 +422,7 @@ function setup(shaders)
     //eixo com duas rodas
     function axisWheels(){
 
-        multRotationZ((-movement/(2*pi*(TORUS_RADIUS/2))*round));
+        multRotationZ((-movement/(2*PI*(TORUS_RADIUS/2))*ROUND));
         multScale([AXIS_WHEEL_SIZE, AXIS_WHEEL_SIZE, AXIS_WHEEL_SIZE]);
 
 
@@ -515,17 +518,17 @@ function setup(shaders)
         }
     }
 
-    function calc(timestamp){
+    function calc(){
 
         for(let i = 0; i < bullets.length; i++){
-            //let time = timestamp - bullets[i].tempo;
-            //let a = mat4(scale(time,acelaration));
             
-            let posfinal = add(bullets[i].velocidade,bullets[i].posicao);
+            let vetorA = mult(time,acelaration);
+            let vetorV = mult(time,bullets[i].velocidade);
+           
+            let posfinal = add(bullets[i].posicao,vetorV);
 
             bullets[i].posicao = posfinal;
-            bullets[i].velocidade = add(bullets[i].velocidade, acelaration);
-            //bullets[i].tempo = timestamp;
+            bullets[i].velocidade = add(bullets[i].velocidade, vetorA);
 
             if(bullets[i].posicao[1][3] <= 0.1){
                 bullets.splice(i,1);
@@ -553,7 +556,7 @@ function setup(shaders)
         popMatrix();
         pushMatrix();
             multTranslation([0,TRANSLATION_TOP_TILES,0])
-            tank(timestamp);
+            tank();
         popMatrix();
 
         
@@ -563,10 +566,8 @@ function setup(shaders)
             bullet();
         }
         
-        
-        calc(timestamp);
-        
-
+        calc();
+    
     }
 }
 
